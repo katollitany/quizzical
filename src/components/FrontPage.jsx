f48377f67333e6ebef1af963d17417b9fd303190
Litany <litany@Litanys-MacBook-Pro.local>
2022-10-28 12:43:20 +0100

    init

---
+++ src/components/FrontPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import IntroPage from "../images/introPage.png";
+
+export default function FrontPage() {
+  return (
+    <div>
+      <h1>{IntroPage}</h1>
+    </div>
+  );
+}
